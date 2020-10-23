bb8ac179aed502d3db611cc5bc86ee138b2b1d1e
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-10-22 21:19:43 -0500

    ajax call finished, no DOM manipulation yet

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,17 +1,16 @@
 //OMDB movie call
 function callMovie() {
-    //var title = "" This needs the users input before it can be implemented
-    var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=41a87e5";
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function(res) {
-        console.log(res)
-        var availableMovies = {};
-        for (var i = )
-        $("<div>").addClass("").attr("id", "movie-list").appendTo("#movie-div");
-        
-    })
+  //var title = "" This needs the users input before it can be implemented
+  var queryURL =
+    "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=41a87e5";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (res) {
+    console.log(res);
+    var availableMovies = {};
+    $("<div>").addClass("").attr("id", "movie-list").appendTo("#movie-div");
+  });
 }
 
 function returnWikiData(killername) {}
@@ -20,13 +19,16 @@ function returnWikiImage(killerimage) {}
 
 function callTv() {}
 
-function callBooks() {}
+function callBooks(killerName) {
+  $.ajax({
+    url:
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      killerName +
+      "+subject",
+    method: "GET",
+  }).then(function (response) {});
+}
 
 function switchScreen() {}
 
 function renderResults() {}
-
-
-
-
-
