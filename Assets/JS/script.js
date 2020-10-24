16caf395856295c67177324cf35447bbe689793e
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-10-23 23:10:06 -0500

    everything adds itself to DOM, carousel not working and cards not showing up.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -48,17 +48,45 @@ function callBooks(killerName) {
       "+subject",
     method: "GET",
   }).then(function (response) {
-    $("<div>").addClass("row").attr("id", "eachBook").appendTo("#main");
-    $.response.items.length.each(function (index, element) {
+    console.log(response);
+    var carousel = $("<div>")
+      .addClass("carousel carousel-slider")
+      .attr("id", "book-carousel");
+    var responseArray = response.items;
+    console.log(responseArray);
+    responseArray.forEach(function (element) {
+      var publishDate = moment(element.volumeInfo.publishedDate, "YYYY-MM-DD");
       $("<div>")
-        .addClass("col s4")
-        .attr("id", "book-card")
-        .val(element.volumeInfo.title)
-        .appendTo("#eachBook");
-      $("<img>")
-        .attr("src", element.volumeInfo.imageLinks.thumbnail)
-        .attr("alt", "Cover for " + element.volumeInfo.title)
-        .appendTo("#book-card");
+        .addClass("carousel-item")
+        .html(
+          '<div class="card"> <div class="card-image waves-effect waves-block waves-light"> <img class="activator" src="' +
+            element.volumeInfo.imageLinks.smallThumbnail +
+            '/400/200" alt="Cover for ' +
+            element.volumeInfo.title +
+            '"> </div> <div class="card-reveal"> <span class=card-title grey-text text-darken-4">' +
+            element.volumeInfo.title +
+            '<i class="material-icons right">close</i></span> <h6>By: ' +
+            element.volumeInfo.authors[0] +
+            "</h6> <h6>Publisher: " +
+            element.volumeInfo.publisher +
+            "</h6> <h6>Date Published: " +
+            publishDate.format("MMMM Do, YYYY") +
+            "</h6> </div>"
+        )
+        .appendTo(carousel);
+      //   .addClass("carousel-item")
+      //   .attr("id", "book-card")
+      //   .val(element.volumeInfo.title)
+      //   .appendTo("#eachBook");
+      // $("<img>")
+      //   .attr("src", element.volumeInfo.imageLinks.thumbnail)
+      //   .attr("alt", "Cover for " + element.volumeInfo.title)
+      // .appendTo("#book-card");
+    });
+    $("<div>").addClass("row").append(carousel).appendTo("#main");
+    document.addEventListener("DOMContentLoaded", function () {
+      var elems = document.querySelectorAll(".carousel");
+      var instances = M.Carousel.init(elems, options);
     });
   });
 }
@@ -66,3 +94,4 @@ function callBooks(killerName) {
 function switchScreen() {}
 
 function renderResults() {}
+callBooks("Ted Bundy");
